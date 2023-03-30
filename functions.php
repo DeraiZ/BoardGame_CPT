e17dd77729bd3a5214411286f93d9b070786c7e0
duilses <matheo.simon46@gmail.com>
2023-03-30 16:00:12 +0200

    add archive page

--- functions.php
+++ functions.php
@@ -8,3 +8,27 @@ function wp_enqueue_assets(){
 }
 
 add_action('wp_enqueue_scripts', 'wp_enqueue_assets');
+
+function game_register_post_type(){
+
+    $labels = array(
+        'name' => 'Jeux',
+        'all_items' => 'Tous les jeux',
+        'singular_name' => 'Jeu',
+        'add_new_item' => 'Ajouter un Jeu',
+        'edit_item' => 'Modifier le Jeu',
+        'menu_name' => 'Jeux'
+    );
+    $args = array(
+      'labels' => $labels,
+      'public' => true,
+      'show_in_rest' => true,
+      'has_archive' => true,
+      'supports' => array('title', 'editor', 'thumbnail'),
+      'menu_position' => 5,
+      'menu_icon' => 'dashicons-admin-customizer'
+    );
+    register_post_type('games', $args);
+}
+
+add_action('init', 'game_register_post_type');
