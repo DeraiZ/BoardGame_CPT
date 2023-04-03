a4aae846017bf12c9841cd5412718333ca995adb
duilses <matheo.simon46@gmail.com>
2023-04-03 15:17:11 +0200

    index modif

--- index.php
+++ index.php
@@ -3,19 +3,22 @@
 get_header();
 
 $args = array(
-    'numberposts' => 9,
+    'numberposts' => 1000,
     'post_type'   => 'games',
     'order'       => 'ASC',
     'orderby'     => 'title'
 );
 $games = get_posts( $args );
+var_dump($games);
 ?>
 <main>
 <?php get_template_part( 'block', 'article' ); 
 if ($games) : ?>
     <ul>
         <?php foreach ( $games as $game ) : setup_postdata( $game ); ?>
-            <li><a href="<?= $game->guid ?>"><?= $game->post_title; ?></a></li>
+            <li>
+                <a href="<?= $game->guid ?>"><?= $game->post_title; ?></a>
+            </li>
         <?php endforeach; wp_reset_postdata(); ?>
     </ul>
 <?php endif; ?>
