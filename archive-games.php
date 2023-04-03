c395b30de73b5988ac4a08354cf8b59e74eda086
clement <clem.jonckheere@gmail.com>
2023-04-03 10:36:54 +0200

    add card

--- archive-games.php
+++ archive-games.php
@@ -1,25 +1,60 @@
 <?php get_header();
 
-$field = array('name','year','date','price','nb_player','time','content','description');
-var_dump(count($field));
-for ($i = 0; $i < count($field); $i++){
-    echo $field[$i];
-}
-?>
+// $field = array('name','year','date','price','nb_player','time','content','description');
+// var_dump(count($field));
+// for ($i = 0; $i < count($field); $i++){
+//     echo $field[$i];
+// }
+// ?>
+<main class="site__recipe">
+<?php get_template_part( 'block', 'article' ); ?>
 <h1 class="site__heading">
     <?php post_type_archive_title(); ?>
 </h1>
-<main class="site__recipe">
-    <?php if (have_posts()) :
-        while (have_posts()) : the_post(); ?>
-            <div class="project">
-                <h2 class="project__title">
-                    <a href="<?php the_permalink(); ?>">
-                        <?php the_title(); ?>
-                    </a>
-                </h2>
-                <?php the_post_thumbnail(); ?>
+            <div class="card-games">
+                <div class="body-card">
+                    <div class="picture-card">
+                        <img src="./img/pagnan.jpg" alt="pagnan">
+                    </div>
+                    <div class="container-card">
+                        <p class="text-card">Pagnan: Le Destin De Roanoke :</p>
+                        <p class="text-card">2023</p>
+                    </div>
+                </div>
+
+                <div class="body-card">
+                    <div class="picture-card">
+                        <img src="./img/pagnan.jpg" alt="pagnan">
+                    </div>
+                    <div class="container-card">
+                        <p class="text-card">Pagnan: Le Destin De Roanoke :</p>
+                        <p class="text-card">2023</p>
+                    </div>
+                </div>
+
+                <div class="body-card">
+                    <div class="picture-card">
+                        <img src="./img/pagnan.jpg" alt="pagnan">
+                    </div>
+                    <div class="container-card">
+                        <p class="text-card">Pagnan: Le Destin De Roanoke :</p>
+                        <p class="text-card">2023</p>
+                    </div>
+                </div>
+
+                <div class="body-card">
+                    <div class="picture-card">
+                        <img src="./img/pagnan.jpg" alt="pagnan">
+                    </div>
+                    <div class="container-card">
+                        <p class="text-card">Pagnan: Le Destin De Roanoke :</p>
+                        <p class="text-card">2023</p>
+                    </div>
+                </div>
+
             </div>
+        <?php if (have_posts()) :
+        while (have_posts()) : the_post(); ?>
         <?php endwhile;
     endif; ?>
 </main>
