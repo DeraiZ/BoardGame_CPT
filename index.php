0a4f7bfced37159e32f36aa6d0ccac837876d740
duilses <matheo.simon46@gmail.com>
2023-04-03 15:22:52 +0200

    get img in index.php

--- index.php
+++ index.php
@@ -14,7 +14,10 @@ $games = get_posts( $args );
     <?php if ($games) : ?>
     <ul>
         <?php foreach ( $games as $game ) : setup_postdata( $game ); ?>
-            <li><a href="<?= $game->guid ?>"><?= $game->post_title; ?></a></li>
+            
+            <li>
+                <img style="width: 5rem" src="<?= get_post_meta($game->ID, '_game_image')[0] ?>">
+                <a href="<?= $game->guid ?>"><?= $game->post_title; ?></a></li>
         <?php endforeach; wp_reset_postdata(); ?>
     </ul>
 <?php endif; ?>
