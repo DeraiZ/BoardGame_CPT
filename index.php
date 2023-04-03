163862af011e5f4bb09fb3e64b86b09ac720bd6b
clement <clem.jonckheere@gmail.com>
2023-04-03 15:25:13 +0200

    add image

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
 
 get_header();
+
+$args = array(
+    'numberposts' => 9,
+    'post_type'   => 'games',
+    'order'       => 'ASC',
+    'orderby'     => 'title'
+);
+$games = get_posts( $args );
 ?>
-
 <main>
-<?php get_template_part( 'block', 'article' ); ?>
+<?php get_template_part( 'block', 'article' ); 
+if ($games) : ?>
+    <ul>
+        <?php foreach ( $games as $game ) : setup_postdata( $game ); ?>
+            <li><a href="<?= $game->guid ?>"><?= $game->post_title; ?></a></li>
+        <?php endforeach; wp_reset_postdata(); ?>
+    </ul>
+<?php endif; ?>
 </main>
-
 <?php
 get_footer();
