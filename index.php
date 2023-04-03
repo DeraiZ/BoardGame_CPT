117319f945fe1e82876df3be886d53c183519bd9
duilses <matheo.simon46@gmail.com>
2023-04-03 11:36:41 +0200

    get title and link

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
 
 get_header();
+
+$args = array(
+    'numberposts' => 9,
+    'post_type'   => 'games',
+    'order'       => 'ASC',
+    'orderby'     => 'title'
+);
+$games = get_posts( $args );
 ?>
-
 <main>
-
+    <?php if ($games) : ?>
+    <ul>
+        <?php foreach ( $games as $game ) : setup_postdata( $game ); ?>
+            <li><a href="<?= $game->guid ?>"><?= $game->post_title; ?></a></li>
+        <?php endforeach; wp_reset_postdata(); ?>
+    </ul>
+<?php endif; ?>
 </main>
-
 <?php
 get_footer();
